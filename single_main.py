fa78ac85d7b6205ec25d8006d3093bda646c4086
harishkannarao <harishkannarao@users.noreply.github.com>
2023-10-09 21:33:07 +0100

    Getting all urls from a blog

--- single_main.py
+++ single_main.py
@@ -1,4 +1,5 @@
 import argparse
+import requests
 from pathlib import Path
 
 my_parser = argparse.ArgumentParser(description='Convert blog post(s) as single epub')
@@ -9,8 +10,22 @@ my_parser.add_argument('--url', '-u', action='store', type=str, required=True)
 
 args = my_parser.parse_args()
 
+def get_all_urls(initial_url):
+    all_urls = []
+    url = initial_url
+    while url is not None:
+        print(url)
+        all_urls.append(url)
+        response = requests.get(url)
+        res_json = response.json()
+        url = next((link['href'] for link in res_json['feed']['link'] if link['rel'] == 'next'), None)
+
+    return all_urls
+
 def convert_to_epub(output_dir, file_name, url):
     Path(output_dir).mkdir(parents=True, exist_ok=True)
+    urls = get_all_urls(url)
+    print(urls)
     return
 
 convert_to_epub(args.dir, args.name, args.url)
