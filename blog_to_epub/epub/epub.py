93a2bae371ce41afcc9e309e1f0761b97ad94ae8
harishkannarao <harishkannarao@users.noreply.github.com>
2023-10-10 10:42:12 +0100

    Using top module and single file flag

---
+++ blog_to_epub/epub/epub.py
@@ -0,0 +1,18 @@
+from blog_to_epub.util.utils import convert_to_epub as convert_epub
+import argparse
+
+my_parser = argparse.ArgumentParser(description='Convert blog post(s) as single epub')
+
+my_parser.add_argument('--dir', '-d', action='store', type=str, required=True)
+my_parser.add_argument('--url', '-u', action='store', type=str, required=True)
+my_parser.add_argument('--single-file', '-sf', action='store_true', required=False)
+my_parser.add_argument('--name', '-n', action='store', type=str, required=False)
+
+args = my_parser.parse_args()
+
+def download_epub():
+    print(args)
+    if args.single_file:
+        print('Single File')
+    convert_epub(args.dir, args.name, args.url)
+    return
--- single_main.py
+++ single_main.py
@@ -1,13 +1,3 @@
-import argparse
+from blog_to_epub.epub.epub import download_epub
 
-from util.utils import convert_to_epub as convert_epub
-
-my_parser = argparse.ArgumentParser(description='Convert blog post(s) as single epub')
-
-my_parser.add_argument('--dir', '-d', action='store', type=str, required=True)
-my_parser.add_argument('--name', '-n', action='store', type=str, required=True)
-my_parser.add_argument('--url', '-u', action='store', type=str, required=True)
-
-args = my_parser.parse_args()
-
-convert_epub(args.dir, args.name, args.url)
+download_epub()
