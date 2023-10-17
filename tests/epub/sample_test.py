fbef0f2d32dcf800d71ea92d868eada0ad52c007
harishkannarao <harishkannarao@users.noreply.github.com>
2023-10-17 11:42:50 +0100

    Adding test with monkeypath to verify pathlib

--- tests/epub/sample_test.py
+++ tests/epub/sample_test.py
@@ -1,5 +1,41 @@
-from assertpy import assert_that
+from pathlib import Path
 
 
-def test_sample():
-    assert_that(True).is_equal_to(True)
+def getssh():
+    """Simple function to return expanded homedir ssh path."""
+    return Path.home() / ".ssh"
+
+
+def create_path(output_dir):
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    return
+
+
+def test_sample(monkeypatch):
+    # mocked return function to replace Path.home
+    # always return '/abc'
+    def mockreturn():
+        return Path("/abc")
+
+    # Application of the monkeypatch to replace Path.home
+    # with the behavior of mockreturn defined above.
+    monkeypatch.setattr(Path, "home", mockreturn)
+
+    x = getssh()
+    assert x == Path("/abc/.ssh")
+
+
+def test_sample2(monkeypatch):
+    mkdir_calls = []
+
+    def mock_mkdir(self: Path, *args, **kwargs):
+        mkdir_calls.append({'path': str(self), 'kwargs': kwargs})
+        return
+
+    monkeypatch.setattr(Path, "mkdir", mock_mkdir)
+
+    create_path("/abc")
+    assert len(mkdir_calls) == 1
+    assert mkdir_calls[0]['path'] == '/abc'
+    assert mkdir_calls[0]['kwargs']['parents'] is True
+    assert mkdir_calls[0]['kwargs']['exist_ok'] is True
