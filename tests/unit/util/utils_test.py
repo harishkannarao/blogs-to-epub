c28559ddc6c731d3f18da20a94b7be83dd8651c9
harishkannarao <harishkannarao@users.noreply.github.com>
2023-10-27 23:35:18 +0100

    Testing retrieval of all chapters

--- tests/unit/util/utils_test.py
+++ tests/unit/util/utils_test.py
@@ -1,13 +1,16 @@
 from assertpy import assert_that
 from ebooklib import epub
 
-from blog_to_epub.util.utils import write_to_epub, get_all_urls
+from blog_to_epub.util.utils import write_to_epub
+from blog_to_epub.util.utils import get_all_urls
+from blog_to_epub.util.utils import get_all_chapters
 from tests.unit.conftest import WriteEpubCall, RequestsGetCall, MockGetResponse
 from tests.unit.response.response_generator import create_blog_response
 
 
 def test__write_to_epub__creates_epub_book(
-        epub_write_fixture: list[WriteEpubCall]):
+        epub_write_fixture: list[WriteEpubCall]
+):
     url = "http://example.com"
     output_dir = "/tmp/path"
     file_name = "some-name"
@@ -32,7 +35,8 @@ def test__write_to_epub__creates_epub_book(
 
 
 def test__get_all_urls__returns_all_urls__from_initial_url(
-        requests_get_fixture: (list[RequestsGetCall], list[MockGetResponse])):
+        requests_get_fixture: (list[RequestsGetCall], list[MockGetResponse])
+):
     get_calls: list[RequestsGetCall] = requests_get_fixture[0]
     get_mock_responses: list[MockGetResponse] = requests_get_fixture[1]
 
@@ -54,3 +58,29 @@ def test__get_all_urls__returns_all_urls__from_initial_url(
     assert_that(get_calls[0].args[0]).is_equal_to('http://example.com')
     assert_that(get_calls[1].args[0]).is_equal_to('http://example.com?page=2')
     assert_that(get_calls[2].args[0]).is_equal_to('http://example.com?page=3')
+
+
+def test__get_all_chapters__returns_all_chapters_from_urls(
+        requests_get_fixture: (list[RequestsGetCall], list[MockGetResponse])
+):
+    get_calls: list[RequestsGetCall] = requests_get_fixture[0]
+    get_mock_responses: list[MockGetResponse] = requests_get_fixture[1]
+
+    get_mock_responses.extend([
+        create_blog_response(chapters=[
+            ('title 2', 'chapter 2'),
+            ('title 1', 'chapter 1'),
+        ]),
+        create_blog_response(chapters=[
+            ('title 4', 'chapter 4'),
+            ('title 3', 'chapter 3'),
+        ]),
+    ])
+
+    result = get_all_chapters(['http://example.com', 'http://example.com?page=2'])
+
+    assert_that(result).is_length(4)
+
+    assert_that(get_calls).is_length(2)
+    assert_that(get_calls[0].args[0]).is_equal_to('http://example.com?page=2')
+    assert_that(get_calls[1].args[0]).is_equal_to('http://example.com')
