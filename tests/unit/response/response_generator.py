a5a3710e137e6bb1e335cb928e6b11b371b49e72
harishkannarao <harishkannarao@users.noreply.github.com>
2023-10-27 23:27:10 +0100

    Creating entries

--- tests/unit/response/response_generator.py
+++ tests/unit/response/response_generator.py
@@ -1,11 +1,24 @@
 from tests.unit.conftest import MockGetResponse
 
 
-def create_blog_response(next_link: str = None) -> MockGetResponse:
+def create_blog_response(
+        next_link: str = None,
+        chapters: list[tuple[str, str]] = None
+) -> MockGetResponse:
     links = []
     if next_link is not None:
         links.append({'rel': 'next', 'href': next_link})
-    feed = {'link': links}
+    entries = []
+    for chapter in chapters or []:
+        title = chapter[0]
+        content = chapter[1]
+        entries.append({
+            'content': {'$t': content},
+            'link': [
+                {'rel': 'alternate', 'title': title}
+            ]
+        })
+    feed = {'link': links, 'entry': entries}
     response = {'feed': feed}
     mock_get_response = MockGetResponse(response)
     return mock_get_response
